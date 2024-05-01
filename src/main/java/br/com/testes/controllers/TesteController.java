d74050b57605bb0dbdfb3f08e0468cdf0732f0b1
Matheus Malta de Aguiar <maltamatheus@gmail.com>
2024-04-30 22:38:49 -0300

    Incluso AuthenticationManager

--- src/main/java/br/com/testes/controllers/TesteController.java
+++ src/main/java/br/com/testes/controllers/TesteController.java
@@ -1,8 +1,16 @@
 package br.com.testes.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.testes.domains.Palavra;
 
 @RestController
 @RequestMapping("/testes")
@@ -17,5 +25,30 @@ public class TesteController {
 	public String teste02() {
 		return "Outro Teste bem sucedido";
 	}
-
+	
+	@GetMapping("/test")
+	public List<String> getArrayInAlphabeticalOrder(@RequestBody List<Palavra> lista){
+		
+		List<String> termos = new ArrayList<String>();
+		
+		System.out.println("Antes da ordenação");
+		
+		for (Palavra palavra : lista) {
+			
+			System.out.println(palavra.getTermo());
+			
+			termos.add(palavra.getTermo());
+		}
+		
+		System.out.println("Depois de ordenar");
+		
+		Collections.sort(termos);
+		
+		for (String string : termos) {
+			System.out.println(string);
+		}
+		
+		return termos;
+	}
+	
 }
---
+++ src/main/java/br/com/testes/domains/Palavra.java
@@ -0,0 +1,8 @@
+package br.com.testes.domains;
+
+import lombok.Data;
+
+@Data
+public class Palavra {
+	private String termo;
+}
