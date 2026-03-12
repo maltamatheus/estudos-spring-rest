61190d6177cde9c273b649ec6e2915f65e2c88ea
Matheus Malta de Aguiar <maltamatheus@gmail.com>
2026-03-12 10:41:25 -0300

    Ajustes base de Dados

--- src/main/java/br/com/testes/domains/Documento.java
+++ src/main/java/br/com/testes/domains/Documento.java
@@ -1,17 +1,16 @@
 package br.com.testes.domains;
 
 import br.com.testes.enums.EnumTipoDocumento;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-
-@Embeddable
 @Getter
 @Setter
+@Embeddable
 public class Documento {
 
     @Enumerated(EnumType.STRING)
--- src/main/java/br/com/testes/domains/Pessoa.java
+++ src/main/java/br/com/testes/domains/Pessoa.java
@@ -1,19 +1,19 @@
 package br.com.testes.domains;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.Embedded;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-@Embeddable
 @Getter
 @Setter
+@Embeddable
 public class Pessoa {
 
     @Column(name = "nome_completo")
